2606e29b1b4b3fe8a030e9c33818ad9ee8a9fd9e
Nils Andreas Svee <nils@stokkdalen.no>
2017-12-06 01:36:10 +0100

    add copy of dnset header

---
+++ src/dnset.h
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2017 by Nils Andreas Svee
+ *
+ * This file is part of dnset.
+ *
+ * dnset is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+bool dnset_match(u8 * group_name, u8 * domain_name);
